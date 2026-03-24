5a9ff44af331524bdfc6fa4e64092aa05212d869
0pFlow <ruslan.galiyev@icloud.com>
2026-03-24 17:56:25 +0100

    vitest config mod

--- vitest.config.js
+++ vitest.config.js
@@ -4,5 +4,10 @@ export default defineConfig({
   test: {
     environment: 'happy-dom',
     include: ['tests/**/*.test.js'],
+    coverage: {
+      provider: 'v8',
+      include: ['js/**/*.js'],
+      exclude: ['js/dom.js'],
+    },
   },
 });
